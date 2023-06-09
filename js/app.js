e2e8f209571ff15c9005c04057f6950dd6a8ea20
Nphnam <nphnamm@gmail.com>
2023-06-09 23:10:07 +0700

    about section

--- js/app.js
+++ js/app.js
@@ -7,4 +7,16 @@ function stickyNavbar(){
 
 }
 
+
 window.addEventListener("scroll",stickyNavbar);
+
+/* ------------------------- Reveal Animation -------------------------- */
+
+let sr = ScrollReveal({
+  duration :2000 ,
+  distance: "60px",
+
+});
+
+sr.reveal(".showcase-info",{delay:600});
+sr.reveal(".showcase-image",{delay:600});
